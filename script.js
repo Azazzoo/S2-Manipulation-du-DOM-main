542bb334a745733c851509b89b5d51e472f4c155
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-01-17 12:55:17 -0500

    ajouter fonction Ajouter(), link css, link scrip

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const panier = {
-    pommes : 2,
+    pommes : 0,
     poires : 0,
     prunes : 0
 }
 
 // Pour afficher le nombre de pommes dans la facture détaillée
 document.querySelector("#qtePommes").textContent = panier.pommes;
+
+function ajouter(choix){
+
+    if (choix === 'pomme')
+    {
+        prix = document.getElementById("prixPommes").innerText;
+        prix = parseInt(prix) + 13.50;
+        document.getElementById("prixPommes").innerText = prix
+        alert("L'item pomme est bien ajouté") // Fait un Pop-up
+    }
+    else if (choix === 'poire')
+    {
+        alert("L'item poire est bien ajouté") // Fait un Pop-up
+    }
+    else
+    {
+        alert("L'item prune est bien ajouté") // Fait un Pop-up
+    }
+
+
+
+}
